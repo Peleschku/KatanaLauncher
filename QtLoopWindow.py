7d99323c0c1890ceb94b81bd24adba25e89eeb79
Adele <adelepeleschka@hotmail.co.uk>
2023-10-14 16:09:10 +0100

    refining the window
    creating a class for that creates my window, as well as creating a button on the window

--- QtLoopWindow.py
+++ QtLoopWindow.py
@@ -1,7 +1,23 @@
-# imports the widgets I need from PyQt5
-from PyQt5.QtWidgets import QApplication, QWidget
-
 import sys
+
+# imports the widgets I need from PyQt5
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class mainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        # Setting the text that appears in the top bar
+        # of the window
+        self.setWindowTitle("Adele's App!")
+        
+        # creates the button and adds text to it
+        button = QPushButton("Press Me!")
+        
+        # centers the button on the window
+        # button created above is passed through as an argument
+        self.setCentralWidget(button)
 
 # creates the application. sys.argv allows you to pass 
 # cmd line arguments through the appliction
@@ -10,8 +26,8 @@ app = QApplication(sys.argv)
 # making the window that opens when the application is
 # launched
 
-window = QWidget()
-window.show()
+window = mainWindow
+window.show() # this line is breaking soemthing, need to check it later.
 
 # starts the application
 app.exec()
