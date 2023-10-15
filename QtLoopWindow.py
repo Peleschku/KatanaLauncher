a187dcd97debce343afb6abcbfd293366ada5515
Adele <133109390+Peleschku@users.noreply.github.com>
2023-10-15 21:31:49 +0100

    messing with window class

--- QtLoopWindow.py
+++ QtLoopWindow.py
@@ -8,26 +8,42 @@ class mainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         
+        # setting window size
+        self.setFixedSize(300, 200)
+
         # Setting the text that appears in the top bar
         # of the window
         self.setWindowTitle("Adele's App!")
         
         # creates the button and adds text to it
-        button = QPushButton("Press Me!")
-        
+        self.button = QPushButton("Press Me!")
+        self.button.setCheckable(True)
+        self.button.released.connect(self.buttonWasReleased)
+        self.buttonIsChecked = False
+        self.button.setChecked(self.buttonIsChecked)
+
         # centers the button on the window
         # button created above is passed through as an argument
-        self.setCentralWidget(button)
+        self.setCentralWidget(self.button)
 
-# creates the application. sys.argv allows you to pass 
+    #functions containting the thing that happens when the button was clicked
+    
+    def buttonWasReleased(self):
+        self.buttonIsChecked = self.button.isChecked()
+
+        print(self.buttonIsChecked)
+
+
+
+# creates the application. sys.argv allows you to pass
 # cmd line arguments through the appliction
 app = QApplication(sys.argv)
 
-# making the window that opens when the application is
-# launched
+# making the window that opens when the application is launched
 
-window = mainWindow
-window.show() # this line is breaking soemthing, need to check it later.
+window = mainWindow()
+window.show()
+
 
 # starts the application
 app.exec()
