81404813632955e002f69719ba9da9703a3ffc4d
Adele <133109390+Peleschku@users.noreply.github.com>
2023-10-23 01:37:33 +0100

    number now increases!
    Managed to get the number to increase as the button is clicked. However, if the number goes above 10, the second digit is cut off >:(

--- increaseButton.py
+++ increaseButton.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QSizePolicy
 
 class buttonWindow(QWidget):
     def __init__(self):
@@ -21,27 +21,36 @@ class buttonWindow(QWidget):
         the label on the UI.
         '''
         nameLable = QLabel(self)
-        nameLable.setText("clikc this button") #sets the text above the window
+        nameLable.setText("click for a surprise") #sets the text above the window
         nameLable.move(60, 30) #positions the button on the UI window
 
         '''
         below:
-        'button' is creating the button with the QPushButton function that's packaged with PyQt
-        'self' being passed is similar to setting a root node in katana - it parents the button the UI window created by the super()
+        _button_ is creating the button with the QPushButton function that's packaged with PyQt
+        _self_ being passed is similar to setting a root node in katana - it parents the button the UI window created by the super()
+
+        adding self before a variable means that the variable can be access from any function within the class.
         '''
-        button = QPushButton("push me!", self)
-        button.clicked.connect(self.buttonClicked) # when button is clicked, call the buttonClicked function
-        button.move(80, 70)
+        self.button = QPushButton("push me!", self)
+        self.button.setCheckable(True)
+        self.button.clicked.connect(self.buttonClicked) # when button is clicked, call the buttonClicked function
+        self.button.move(60, 60)
+        
+        self.numberCount = QLabel(self)
+        self.currentNumber = 0
+        self.numberCount.setNum(self.currentNumber)
+        self.numberCount.move(95, 100)
+        self.numberCount.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum) # trying to get two numbers to show when the list hits 10 +
 
     def buttonClicked(self):
-        numberLabel = QLabel(self)
-        if self.buttonClicked == True:
-            numberLabel.setText()
+        
+        self.currentNumber += 1 # takes the currentNumber variable, adds one and then stores the new value in the variable
+
+        if self.button.isCheckable():
+            self.numberCount.setNum(self.currentNumber)
+  
 
 app = QApplication(sys.argv)
 
 myWindow = buttonWindow()
 sys.exit(app.exec_())
-
-
-    
