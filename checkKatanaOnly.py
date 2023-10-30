876223abe984048f4cf62c879bc0fbba14b9c709
Adele <adelepeleschka@hotmail.co.uk>
2023-10-30 09:32:41 +0000

    figuring a way to launch katana

--- checkKatanaOnly.py
+++ checkKatanaOnly.py
@@ -1,6 +1,9 @@
 import os
 from os.path import isfile, join
 
+'''
+Making sure that only Katana installs are being detected in the
+intstalls directory, and not any other installs fninstall may have made.
 
 checkDirectory = os.listdir('C:\Program Files\Foundry')
 prefix = 'Katana'
@@ -14,6 +17,6 @@ for k in checkDirectory:
     else:
         continue
 print(katanaInstalls)
-
+'''
 
 
